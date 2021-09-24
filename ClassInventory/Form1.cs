d14371223d6c71cf36c462c27c49a1705d4b7639
Sam Wolfgram <samuwolf401@outlook.com>
2021-09-23 23:29:02 -0400

    sending over

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -13,6 +13,7 @@ namespace ClassInventory
     public partial class Form1 : Form
     {
         // TODO - create a List to store all inventory objects
+        List<TeamData> data = new List<TeamData>();
 
         public Form1()
         {
@@ -22,12 +23,22 @@ namespace ClassInventory
         private void addButton_Click(object sender, EventArgs e)
         {
             // TODO - gather all information from screen 
-
+            string name = nameInput.Text;
+            int age = Convert.ToInt32(ageInput.Text);
+            string team = teamInput.Text;
+            string position = positionInput.Text;
             // TODO - create object with gathered information
-
+            TeamData tempData = new TeamData(age, name, team, position);
             // TODO - add object to global list
-
+            data.Add(tempData);
             // TODO - display message to indicate addition made
+            outputLabel.Text = $"{name} Added To The Database Of Champions";
+
+            nameInput.Text = "";
+            ageInput.Text = "";
+            teamInput.Text = "";
+            positionInput.Text = "";
+            
         }
 
         private void removeButton_Click(object sender, EventArgs e)
@@ -46,14 +57,19 @@ namespace ClassInventory
             // This is to be completed in Part II. You will use 
             // Lambda Expressions.
             //---------------------------
-
             // TODO - if object entered exists in list show it
             // TODO - else show not found message
+
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
             // TODO - show all objects in list. Use a foreach loop.
+            outputLabel.Text = "";
+            foreach (TeamData d in data)
+            {
+                outputLabel.Text += $"{d.name}, {d.age}, {d.team}, {d.position}\n";
+            }
         }
     }
 }
---
+++ ClassInventory/TeamData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInventory
+{
+    class TeamData
+    {
+        public string name, team, position;
+        public int age;
+
+        public TeamData(int _age, string _name, string _team, string _position)
+        {
+            name = _name;
+            age = _age;
+            team = _team;
+            position = _position;
+
+
+        }
+
+        
+
+
+        
+
+    }
+}
